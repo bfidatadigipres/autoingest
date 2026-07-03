8e69f53e4e5b4ebf76fa83ed5e0bd74415cb9bbf
digitensions <pittschick@me.com>
2026-07-03 11:54:19 +0100

    Fix broken link in metadata updater

--- autoingest/ops/local/cid_metadata_update.py
+++ autoingest/ops/local/cid_metadata_update.py
@@ -225,7 +225,7 @@ def update_cid_metadata(context: OpExecutionContext) -> Output:
         cid_toc = time.perf_counter()
         cid_update_time = round(cid_toc - cid_tic, 3)
     else:
-        context.log.warning(f"Failed to push metadata to header tags in rec {media_priref}:\n{payload}")
+        context.log.warning(f"Failed to push metadata to header tags in rec {media_priref}:\n{payload_data}")
         cid_update_time = 0
 
     mime_type = (mime_type or "").lower()
