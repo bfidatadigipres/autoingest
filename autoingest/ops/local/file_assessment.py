711dade1861325c1bfd013bea102476818d10ce1
digitensions <pittschick@me.com>
2026-09-18 16:49:22 +0100

    Enhancement: reorganise point file is assessed as multipart and remove warning from logs - just skip quietly if not possible.

--- autoingest/ops/local/file_assessment.py
+++ autoingest/ops/local/file_assessment.py
@@ -100,6 +100,16 @@ def assess_filename(context: OpExecutionContext) -> Output:
             do_ingest = False
         elif isinstance(previous_part, str):
             pp_field_details = db.lookup_file_details(previous_part)
+            if pp_field_details[2] == "No Status":
+                context.log.info(f"Skipping ingest - previous part has not been ingested yet")
+                db.update_file_status(field_details[0], file_status="No Status")
+                return Output(
+                    {},
+                    metadata={
+                        "duration_sec": round(time.perf_counter() - tic, 3),
+                        "preview": f"Previous part not yet ingested: {filename} - previous part {previous_part}",
+                    },
+                )
             if not pp_field_details:
                 context.log.info(f"Skipping ingest - previous part has not been ingested yet")
                 db.update_file_status(field_details[0], file_status="No Status")
