5c0e0adfc7d52d623ae8fbc4df052e282ae55996
digitensions <pittschick@me.com>
2026-09-18 16:35:54 +0100

    Remove need to print previous part metadata out.

--- autoingest/ops/local/file_assessment.py
+++ autoingest/ops/local/file_assessment.py
@@ -100,7 +100,6 @@ def assess_filename(context: OpExecutionContext) -> Output:
             do_ingest = False
         elif isinstance(previous_part, str):
             pp_field_details = db.lookup_file_details(previous_part)
-            context.log.info(pp_field_details)
             if not pp_field_details:
                 context.log.info(f"Skipping ingest - previous part has not been ingested yet")
                 db.update_file_status(field_details[0], file_status="No Status")
