99a91e1ee0163b7bacd2c7377ce781eb579b1b50
digitensions <pittschick@me.com>
2026-09-23 14:40:43 +0100

    Enhancement: Error message for No Status missing parts

--- autoingest/ops/local/file_assessment.py
+++ autoingest/ops/local/file_assessment.py
@@ -102,7 +102,7 @@ def assess_filename(context: OpExecutionContext) -> Output:
             pp_field_details = db.lookup_file_details(previous_part)
             if not pp_field_details:
                 context.log.info(f"Skipping ingest - previous part has not been ingested yet")
-                db.update_file_status(field_details[0], file_status="No Status")
+                db.update_file_status(field_details[0], file_status="No Status", error_message="Waiting for previous part to ingest")
                 return Output(
                     {},
                     metadata={
